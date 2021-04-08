f43bb031f10322dfbe3fd51b44934bf758a846ca
othaderek <otha.hernandez@gmail.com>
2021-04-08 11:26:31 -0400

    Cleaned up app component and removed test hooks.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import { useState, useEffect } from 'react'
 import PlayButton from './components/buttons/PlayButton'
 
 function App() {
-  let [count, setCount] = useState(0)
-
-  useEffect(() => {
-    console.log(`side effect and ${count}`)
-  }, [count])
-
-  const handleClick = (e) => {
-    if (e.target.id === 'increment') setCount((count += 1))
-    if (e.target.id === 'decrement') setCount((count -= 1))
-  }
-
   return (
     <div className='App'>
       <PlayButton />
