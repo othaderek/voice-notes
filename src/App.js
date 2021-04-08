8e219cb61edb51aa2c0c27a6a5282de6e21881ce
othaderek <otha.hernandez@gmail.com>
2021-04-08 11:33:44 -0400

    Added record and delete buttons.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
+import DeleteButton from './components/buttons/DeleteButton'
 import PlayButton from './components/buttons/PlayButton'
+import RecordButton from './components/buttons/RecordButton'
 
 function App() {
   return (
     <div className='App'>
       <PlayButton />
+      <RecordButton />
+      <DeleteButton />
     </div>
   )
 }
--- src/components/buttons/RecordButton.js
+++ src/components/buttons/RecordButton.js
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import withStyles from '@material-ui/core/styles/withStyles'
-import yellow from '@material-ui/core/colors/yellow'
+import red from '@material-ui/core/colors/red'
 
 const styles = (theme) => ({
-  containedYellow: {
-    color: theme.palette.getContrastText(yellow[500]),
-    backgroundColor: yellow[500],
+  containedRed: {
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
     width: '200px',
     height: '80px',
   },
 })
 
-function DeleteButton(props) {
+function RecordButton(props) {
   const { classes } = props
   // clickedOn state that tracks if it is on or off
   let [clickedOn, setClickedOn] = useState(false)
   // Ficgure out even handlers in Material UI
-  return <Button className={classes.containedYellow}>Delete</Button>
+  return <Button className={classes.containedRed}>Record</Button>
 }
 
-export default withStyles(styles)(DeleteButton)
+export default withStyles(styles)(RecordButton)
