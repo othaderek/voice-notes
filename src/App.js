e356bbc3f969a12ca849552cdd8412c1e255e4f5
othaderek <otha.hernandez@gmail.com>
2021-04-08 10:52:37 -0400

    Added material ui.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  let [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log(`side effect and ${count}`)
+  }, [count])
+
+  const handleClick = (e) => {
+    if (e.target.id === 'increment') setCount((count += 1))
+    if (e.target.id === 'decrement') setCount((count -= 1))
+  }
+
   return (
     <div className='App'>
       <marquee behavior='' direction=''>
-        hi
+        <>{count}</>
       </marquee>
+      <button id='increment' onClick={handleClick}>
+        increment
+      </button>
+      <button id='decrement' onClick={handleClick}>
+        decrement
+      </button>
     </div>
   )
 }
